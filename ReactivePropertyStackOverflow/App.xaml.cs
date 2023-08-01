0e6881d64dd86d72566263c459f84363ff2c2ac3
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2023-08-02 07:56:26 +0900

    first

---
+++ ReactivePropertyStackOverflow/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Schedulers;
+using System.Windows;
+
+namespace ReactivePropertyStackOverflow;
+
+public partial class App
+{
+    private void App_OnStartup(object sender, StartupEventArgs e)
+    {
+        ReactivePropertyScheduler.SetDefault(new ReactivePropertyWpfScheduler(Dispatcher));
+    }
+}
---
+++ ReactivePropertyStackOverflow/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Reactive.Bindings;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReactivePropertyStackOverflow;
+
+public partial class MainWindow
+{
+    private readonly ObservableCollection<int> _source = new();
+    private readonly ReadOnlyReactiveCollection<int> _target;
+        
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        _target = _source.ToReadOnlyReactiveCollection(x => x);
+    }
+    
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        Task.Run(() =>
+        {
+            for (var i = 0; i != 10000; ++i)
+            {
+                _source.Add(i);
+            }
+        });
+    }
+}
